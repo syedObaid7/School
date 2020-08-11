d96d1044526f6313a2db96edd768075154c517fd
syedObaid7 <69494829+syedObaid7@users.noreply.github.com>
2020-08-10 22:23:04 -0500

    Add files via upload

---
+++ SQ55_A1EX1.cpp
@@ -0,0 +1,12 @@
+//  Gradebook #55
+// The program displays my first and last name in a box
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*-----------*\n|Syed Quadri|\n*-----------*" << endl;
+
+    return 0;
+}
